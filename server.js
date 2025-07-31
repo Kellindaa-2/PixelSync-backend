e28dff86539e3513f21cdc0062a045fe347b4ddd
Kelline Lubrun <kslubrun@gmail.com>
2025-07-31 14:41:56 -0400

    Update server.js and routes

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ import path from "path";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import authRoutes from "./routes/auth.js";
+import dashboardRoutes from "./routes/dashboard.js";
+import expressLayouts from 'express-ejs-layouts';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -18,8 +20,14 @@ app.use(express.static(path.join(__dirname, '../PixelSync-frontend/public')));
 app.set('views', path.join(__dirname, '../PixelSync-frontend/views'));
 app.set('view engine', 'ejs');
 
+app.use(expressLayouts);
+app.set('layout', 'layout');
+
 // Use auth routes
 app.use('/auth', authRoutes);
+
+// Dashboard routes
+app.use('/dashboard', dashboardRoutes);
 
 // Default route - redirect to login
 app.get('/', (req, res) => {
